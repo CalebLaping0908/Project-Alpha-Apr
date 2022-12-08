08ebd2393d5e32faf6cb3f7863af437f377ee1dd
Caleb <cmlaping@gmail.com>
2022-12-08 10:51:10 -0700

    Feature 8 and 9 complete

--- accounts/views.py
+++ accounts/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render, redirect
 from accounts.forms import LoginForm
-from django.contrib.auth import login, authenticate
+from django.contrib.auth import login, authenticate, logout
 
 
 def user_login(request):
@@ -25,3 +25,10 @@ def user_login(request):
         "form": form,
     }
     return render(request, "accounts/login.html", context)
+
+
+def user_logout(request):
+
+    if request.user is not None:
+        logout(request)
+        return redirect("login")
